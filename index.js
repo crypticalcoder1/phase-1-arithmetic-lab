11875e9c92d26e9bf115531782aacf5afec52142
crypticalcoder1 <kirambawycliff@gmail.com>
2024-07-03 15:31:50 +0300

    mt file

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100); // Generates a random integer between 1 and 100
+
+let num3 = 40;
+let num4 = 36;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 10, 15, 20); // Finds the maximum value in the provided list
